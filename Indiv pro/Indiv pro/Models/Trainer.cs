37bfd959705d0a9663f8523c83a480bc44a160b3
NikosSalichos <70713775+Nikos-Salichos@users.noreply.github.com>
2021-05-07 11:05:49 +0300

    Add files via upload

---
+++ Indiv pro/Indiv pro/Models/Trainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indiv_pro
+{
+    class Trainer
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _subject;
+
+        public string firstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string lastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+        public Trainer(string firstName, string lastName, string subject) //constructor
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.subject = subject;
+        }
+
+    }
+}
---
+++ Indiv pro/Indiv pro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel;
+
+namespace Indiv_pro
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            School School = new School();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
